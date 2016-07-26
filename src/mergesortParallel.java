fbd68feca05fe922092bb3cbc764ab6d689ecfd4
m-rashid <mustafa.rashidZA@gmail.com>
2016-07-26 20:50:14 +0200

    Made the code more compact by removing the size() method, and hardcoding it into the getLarger() method

--- src/mergesortParallel.java
+++ src/mergesortParallel.java
@@ -8,6 +8,61 @@
  *
  * @author merashid27
  */
-public class mergesortParallel {
+
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+public class mergesortParallel extends RecursiveAction{
+   
+    int threshold;
     
+    int start;
+    int end;
+    int[] arr;
+    mergesortParallel left;
+    mergesortParallel right;
+    
+    public mergesortParallel(int[] arr, int start, int end, int threshold){
+        this.arr = arr;
+        this.start = start;
+        this.end = end;
+        this.threshold = threshold;
+        
+        
+    }
+    
+    @Override
+    public void compute(){
+        int middle = start + (end-start)/2;
+        
+        if(end-start <= threshold){
+            Arrays.sort(arr, start, end);    
+            return;
+        }
+        
+        invokeAll(
+               left = new mergesortParallel(arr, start, middle, threshold),
+               right = new mergesortParallel(arr, middle, end, threshold) 
+        );
+        sequentialMerge(middle);
+            
+    }
+    
+    public void sequentialMerge(int middle){
+        
+        
+        
+    }
+    
+    public int size(){
+       return end-start;
+    }
+    
+    public mergesortParallel getLarger(){
+        if(left.size()>right.size()){
+            return left;
+        }
+        return right;
+    }
 }
