abbffb82b8253596e914264f1f5485067bc7c4cb
m-rashid <mustafa.rashidZA@gmail.com>
2016-07-30 22:07:02 +0200

    Changed the temp[] array size in sequentialMerge() method to (end-start)

--- src/mergesortParallel.java
+++ src/mergesortParallel.java
@@ -12,6 +12,7 @@
 import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
+import java.util.*;
 
 public class mergesortParallel extends RecursiveAction{
    
@@ -19,7 +20,7 @@ public class mergesortParallel extends RecursiveAction{
     
     int start;
     int end;
-    int[] arr;
+    static int[] arr;
     mergesortParallel left;
     mergesortParallel right;
     
@@ -42,40 +43,130 @@ public class mergesortParallel extends RecursiveAction{
         }
         
         invokeAll(
-               left = new mergesortParallel(arr, start, middle, threshold),
-               right = new mergesortParallel(arr, middle, end, threshold) 
+               new mergesortParallel(arr, start, middle, threshold),
+               new mergesortParallel(arr, middle, end, threshold) 
         );
         sequentialMerge(middle);
+        
+        
             
     }
     
+    /*
     public void sequentialMerge(int middle){
-        int largerMid = (getLarger().size())/2;
         
-        int smallerMidPos = Arrays.binarySearch(getSmaller().arr, largerMid);
+        int[] temp = new int[arr.length];
+        int tempStart = 0;
+        int leftStart = start;
+        int rightStart = middle;
+        int rightEnd = end;
+        int leftEnd = middle-1;
         
-        
-        
-        
-        
-    }
-    
-    public int size(){
-       return end-start;
-    }
-    
-    public mergesortParallel getLarger(){
-        if(left.size()>right.size()){
-            return left;
+        while(leftStart<=leftEnd && rightStart<=rightEnd){
+            if(arr[leftStart]<=arr[rightStart]){
+                temp[tempStart++] = arr[leftStart++];
+            }
+            else{
+                temp[tempStart++] = arr[rightStart++];
+            }
         }
-        return right;
+        
+        while(leftStart<=leftEnd){
+            temp[tempStart++] = arr[leftStart++];
+        }
+        
+        while(rightStart<=rightEnd){
+            temp[tempStart++] = arr[rightStart++];
+        }
+        
+        /*
+        for(int i = arr.length-1 ; i>0; i--, rightEnd--){
+            arr[rightEnd]=temp[i];
+        }
+        
+        
+        for(int i : temp){
+            System.out.println(i);
+        }
+    }
+    */
+    
+    //copy to temp[]
+    
+    public void sequentialMerge(int middle){
+        int temp[] = new int[arr.length];
+        
+        //copy everything into temp[]
+        for(int i = start; i<=end; i++){
+            temp[i] = arr[i];
+            
+            
+        }
+        
+        
+        
+        int i = start;
+        int j = middle+1;
+        int k = start;
+        
+        //i<=middle && j<=end
+        while(true){
+            while(i<=middle){
+                 arr[k] = temp[i];
+               
+            k++;
+            i++;
+            }
+            
+            while(j<end){
+                arr[k] = temp[j];
+                //k++;
+               j++;
+            }
+            
+            if(temp[i] <= temp[j]){
+                arr[k] = temp[i];
+               // i++;
+            }
+            else{
+                arr[k] = temp[j];
+                j++;
+            }
+            k++;
+        }
+        
+        //whatever remains in the left array
+        /*
+        while(i<=middle){
+            arr[k] = temp[i];
+            k++;
+            i++;
+        }
+        */
+        /*
+        for(int m : temp){
+            System.out.println(m);
+        }
+        */
         
     }
     
-    public mergesortParallel getSmaller(){
-        if(left.size()<right.size()){
-            return left;
+    
+    
+    public static void main(String[] args){
+        int[] array = {4,1,2,7,8,6,12,11}; //20 elements
+        
+        mergesortParallel sort = new mergesortParallel(array, 0, array.length-1, 4);
+        
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(sort);
+        
+        for(int i : arr){
+            System.out.println(i);
         }
-        return right;
+                
+                
     }
+    
+
 }
